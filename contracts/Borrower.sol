3a5ab4beec2cff229d2782a2bb193149c628d56b
Michael <user@Users-MacBook-Pro.local>
2022-06-20 09:27:38 -0500

    feat: main functionality interface created

---
+++ contracts/Borrower.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./MyNFT.sol";
+import "hardhat/console.sol";
+
+contract Borrower is Context {
+}
---
+++ contracts/MyNFT.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721("MyNFT", "nft"), ERC721Enumerable, Ownable {
+    constructor() {
+        
+    }
+}
